8f7f6d64e9b448f1a9dd5cd4bbed97daa00bdd10
LikeMyBread <mbradley31@outlook.com>
2019-03-12 20:16:01 -0700

    Added soulnames

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import soulName from './helpers/name/soulName';
+import { capitalizeFirst } from './helpers/utils';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src="img/sprites/chirling/saylian.png" />
+        <img src="img/sprites/chirling/saylian.png" alt="chirling" />
+        <h2>{capitalizeFirst(soulName())}</h2>
       </div>
     );
   }
