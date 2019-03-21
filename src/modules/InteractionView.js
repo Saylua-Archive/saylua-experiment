ca58c3732de388ae750f0be0b28ffe0574baa182
tiffz <zhangtiff@gmail.com>
2019-03-20 23:59:55 -0700

    Add some boilerplate.

---
+++ src/modules/InteractionView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './InteractionView.css';
+
+export default function InteractionView(props) {
+  return (
+    <div className="wilderness-background">
+    </div>
+  );
+}
