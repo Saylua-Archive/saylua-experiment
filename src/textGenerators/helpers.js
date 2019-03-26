4da46ca552b8165397a50a561c6750e7876136a0
tiffz <zhangtiff@gmail.com>
2019-03-25 22:39:36 -0700

    Fix plural vs. singular bug.

--- src/textGenerators/helpers.js
+++ src/textGenerators/helpers.js
@@ -5,7 +5,7 @@ export const pN = (singular, pluralArg) => {
 }
 
 // Pluralize a verb based on a subject object.
-export const pV = (singular, pluralArg) => {
-  const plural = pluralArg || `${singular}s`;
+export const pV = (plural, singularArg) => {
+  const singular = singularArg || `${plural}s`;
   return (subject) => subject.grammar.isPlural ? plural : singular;
 }
