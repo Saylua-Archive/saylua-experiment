11922f5aaf6a91769639b8e88673431017acbb30
tiffz <zhangtiff@gmail.com>
2019-04-06 23:59:49 -0700

    Add trivial character display

--- src/modules/CityView.js
+++ src/modules/CityView.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './WildernessView.css';
 
-export default function CityView(props) {
-  const { region, className } = props;
+export default class CityView extends Component {
+  constructor(props) {
+    super(props);
 
-  const bgStyle = `url('img/wilderness/${region.canonName}.jpg')`;
+    this.state = {
+      character: '',
+    };
+  }
 
-  return (
-    <div
-      className={`wilderness-background ${className}`}
-      style={{
-        backgroundImage: bgStyle,
-      }}
-    >
-      { region.name }
-    </div>
-  );
+  changeCharacter(character) {
+    this.setState({ character });
+  }
+
+  render() {
+    const { region, className } = this.props;
+    const { character } = this.state;
+
+    const bgStyle = `url('img/wilderness/${region.canonName}.jpg')`;
+    const characterImage = character ? `img/characters/${character}.png` : '';
+
+    return (
+      <div className="interaction-container">
+        <div
+          className={`interaction-view wilderness-background ${className}`}
+          style={{
+            backgroundImage: bgStyle,
+          }}
+        >
+          { characterImage
+            && <img src={characterImage} alt={character} title={character} />
+          }
+        </div>
+        <div className="interaction-content">
+          <h2>{ region.name }</h2>
+          <p>
+            You see some people in wandering around the city. Who will you talk to?
+          </p>
+          <button type="button" onClick={() => this.changeCharacter('vera')} className="button">
+            Vera Everly
+          </button>
+          <button type="button" onClick={() => this.changeCharacter('rufus')} className="button">
+            Rufus Scippio
+          </button>
+          <button type="button" onClick={() => this.changeCharacter('luana')} className="button">
+            Luana Liddic
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 CityView.propTypes = {
